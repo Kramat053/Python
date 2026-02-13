7cdd74f2c9b649e6994adb4d633af88ce7d7b566
Kramat053 <karamat2053@gmail.com>
2026-02-13 22:48:44 +0500

    File name change

---
+++ Functions.py
@@ -0,0 +1,19 @@
+#Functions in Python
+print("Hello")
+a = input("Enter your name")
+type(a)#Returns data type of any variable
+int(a)#used for type conversion
+abs(4) #Absolute Function
+pow(2,3) #Power Function
+min([1,2,3,4])
+max([1,2,3,4])
+round(a) #To round of a float value
+divmod(5,2) #It takes two inputs and returns the quotient and the remainder
+bin(4)#Converts any number to binary
+oct(4)#Converts any number to octal
+hex(4)#Converts any number to hexadecimal
+id(a)#To find the memory address of a variable
+ord("a")#Returns the ASCII value
+len(a)#Returns length of a value
+sum()#Returns sum of values
+help("print")#Returns documentation of any function
