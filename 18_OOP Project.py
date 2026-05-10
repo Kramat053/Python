9439ae73b29a02b97ce0e565d63bfb1fa2d2f572
Kramat053 <karamat2053@gmail.com>
2026-05-10 23:39:32 +0500

    aa

---
+++ 18_OOP Project.py
@@ -0,0 +1,16 @@
+class Starship:
+    def __init__(self,fuel):
+        self._fuel = fuel
+    def calculate_warp_speed(self):
+     pass
+
+class Freighter(Starship):
+    def __init__(self):
+         pass
+    def calculate_warp_speed(self):
+        
+class Fighter(Starship):
+    def __init__(self):
+        pass
+    def calculate_warp_speed(self):
+            
