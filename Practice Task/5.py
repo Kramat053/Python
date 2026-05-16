e8eb28fb133ea405c489b3a2d910a4dbbd073ced
Kramat053 <karamat2053@gmail.com>
2026-05-16 16:41:10 +0500

    a

--- Practice Task/5.py
+++ Practice Task/5.py
@@ -1,3 +1,10 @@
 #Reversing a number
 num = 1234
-print(str(num)[:-1])
+reversed_num = (str(num)[::-1])
+print("Actual number is",num)
+print("Reversed number is",reversed_num)
+if str(num) == reversed_num:
+    print("The reverse is not true")
+else:
+    ("The reverse is true")    
+
